24339fca937a1dde4b595ba64be778cec31100d0
Pablo Peralta <pablodiedrich@hotmail.com>
2025-12-05 23:36:18 -0300

    glm::dot -> dotproduct

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -3,7 +3,8 @@
 #include "../include/matrices.h"
 
 bool checkSpherePlaneCollision(const Sphere& sphere, const Plane& plane) {
-    float signedDistance = glm::dot(plane.normal, sphere.center) + plane.distance;
+    float signedDistance = dotproduct(glm::vec4(plane.normal, 0.0f),
+                                         glm::vec4(sphere.center, 0.0f)) + plane.distance;
     return glm::abs(signedDistance) <= sphere.radius;
 }
 
